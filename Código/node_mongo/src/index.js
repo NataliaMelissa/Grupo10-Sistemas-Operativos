f19ecb3f89c659e7492e6da164da967d8102ecdc
NataliaMelissa <u201816996@upc.edu.pe>
2023-11-05 21:48:38 -0500

    CRUD users

--- Código/node_mongo/src/index.js
+++ Código/node_mongo/src/index.js
@@ -6,5 +6,15 @@ console.log("(index.js) Variables app y express creadas y configuradas...");
 require("./database"); //Conozca el archivo database.js para que llame a la BD
 console.log("(index.js) Se configuró la BD");
 
+//Llamar los elemenos que requiere la aplicación:
+app.use(express.json()); //Llamar a json para poder hacer los mapeos
+console.log("(index.js) se indicó que se debe utilizar express.json");
+
+app.use(require("./routes/index.routes.js")) //Llamar a los servicios
+console.log("(index.js) se llamó al archivo routes.js");
+
+app.use(require("./userModel.js")); //Llamar al userModel
+console.log("(index.js) se llamó al archivo userModel.js");
+
 app.listen(3000); //Indicar a la aplicación que escuche ese puerto para el servicio web
 console.log("(index.js) Se configuró la aplicación para que escuche al puerto 3000");
